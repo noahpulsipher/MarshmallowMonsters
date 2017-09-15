4a5045a3025d7d671e88ee0147371e00adfdfea3
noahpulsipher <31518741+noahpulsipher@users.noreply.github.com>
2017-09-15 12:11:42 -0600

    I made a toString method

--- src/monster/model/MarshmallowMonster.java
+++ src/monster/model/MarshmallowMonster.java
@@ -1,5 +1,37 @@
 package monster.model;
 
-public class MarshmallowMonster {
+public class MarshmallowMonster 
+{
+	//Declaration section of data members
+	
+	private String name;
+	private int eyeCount;
+	private int armCount;
+	private double tentacleAmount;
+	private boolean hasBloop;
+	
+	
+	public MarshmallowMonster()
+	{
+		//initializes everything to 0, false or null.
+	}
 
+	public MarshmallowMonster(String name, int eyeCount, int arms, int tentacleCount, boolean hasBloop)
+	{
+		this.name = name;
+		this.eyeCount = eyeCount;
+		this.armCount = arms;
+		this.tentacleAmount = tentacleCount;
+		this.hasBloop = hasBloop;
+		
+	}
+
+	public String toString()
+	{
+		String description = "I am a monster, my name is " + name + ", I have " + eyeCount + " eyes and only" ;
+		description += tentacleAmount + " tenticles but I have " + armCount + " arms!." + hasBloop + "tells you";
+		description += "if I have a bloop";
+		return description;	
+	}
 }
+
